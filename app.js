38c9754bd93f3a41e148b32962b6de9ed6df2d3e
greatwu09 <di_wu@keysight.com>
2017-01-18 17:15:43 +0800

    20170118 change app.js

--- app.js
+++ app.js
@@ -1,71 +1,17 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/route');
-var api = require('./routes/api');
-var app = express();
-//20170118
+var express = require("express");
 var mysql = require('mysql');
+var app = express();
+app.use(express.logger());
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'ejs');
-// ����ģ���ļ�����չ��Ϊhtml
-app.engine('html', require('ejs').renderFile); // ��ejsģ������������'.html'��׺���ļ�
-app.set('view engine', 'html');
+/*
+ var db_config = {
+ host: 'us-cdbr-east-04.cleardb.com',
+ user: 'b6d6c6e8740d20',
+ password: 'b3f75ada',
+ database: 'heroku_1daa39da0375291'
+ };
+ */
 
-// ����public�ļ�Ŀ¼�ʾ�̬Ŀ¼
-app.use(express.static(path.join(__dirname, 'public'),{index:false}));
-app.use(express.static(path.join(__dirname, 'common'),{index:false}));
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/api', api);
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-//20170118
 var db_config = {
   host: 'us-cdbr-iron-east-04.cleardb.net',
   user: 'bcaf52a66bd308',
@@ -75,7 +21,6 @@ var db_config = {
 
 
 var connection;
-var dataRows;
 
 function handleDisconnect() {
   console.log('1. connecting to db:');
@@ -99,26 +44,19 @@ function handleDisconnect() {
   });
 }
 
-function getDataFromDB(){
+handleDisconnect();
+
+app.get('/', function(request, response) {
   connection.query('SELECT * from employees', function(err, rows, fields) {
     if (err) {
       console.log('error: ', err);
       throw err;
     }
-
-    return rows;
+    response.send(['Hello World!!!! HOLA MUNDO!!!!', rows]);
   });
-}
+});
 
-handleDisconnect();
-dataRows = getDataFromDB();
-
-
-exports.dataRows = dataRows;
-
-
-var serverPort = process.env.PORT || 5000;
-app.listen(serverPort);
-console.log("0000");
-
-module.exports = app;
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
