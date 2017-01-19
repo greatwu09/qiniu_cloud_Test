92d21b6250cbf21ce0d12a751795079ed8b60b44
greatwu09 <di_wu@keysight.com>
2017-01-19 09:32:13 +0800

    20170119v1

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    'ACCESS_KEY': 'g-jeMQxivZL1JNTCzx0jQE-dGw-TBNZBV4wiz-RH',   // 七牛个人账户公钥
-    'SECRET_KEY': 'i04H_5Sb5i5BjnK_4aoiDChO6f0wORHOMpbiuM8c',   // 七牛个人账户私钥
-    'Bucket_Name': 'myplan',                             // 七牛-个人空间名
-    'Port': 19111,
-    'Uptoken_Url': '/api/fileupload/uptoken',               // 获取token路径
-    //'Domain': '7xqqm5.com1.z0.glb.clouddn.com'    // 七牛子域名，自定义域名。
-    'Domain': 'oin5dqv2h.bkt.clouddn.com'
-};
